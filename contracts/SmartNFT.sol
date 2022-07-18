2251fe2ec98a420afc0ae4da548e5ab4f84e7c12
hemantjuyal <hemantjuyal@gmail.com>
2022-07-18 14:01:44 +0530

    smart contract for nft

---
+++ contracts/SmartNFT.sol
@@ -0,0 +1,23 @@
+// contracts/SmartNFT.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract SmartNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("SmartNFT", "SMRT") {}
+
+    function publishNFT(address user, string memory tokenURI) public onlyOwner returns (uint256){
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(user, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+}
